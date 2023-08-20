a12e3311f87f41491a7f0636a03b4ebde5c5f9ba
Stefan Melmuk <stefan.melmuk@gmail.com>
2023-08-20 19:17:39 +0200

    add revised checkout script

---
+++ scripts/checkout_web_vault.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+set -o pipefail -o errexit
+BASEDIR=$(RL=$(readlink -n "$0"); SP="${RL:-$0}"; dirname "$(cd "$(dirname "${SP}")"; pwd)/$(basename "${SP}")")
+
+# Error handling
+handle_error() {
+    read -n1 -r -p "FAILED: line $1, exit code $2. Press any key to exit..." _
+    exit 1
+}
+trap 'handle_error $LINENO $?' ERR
+
+# Load default script environment variables
+# shellcheck source=.script_env
+. "${BASEDIR}/.script_env"
+
+# Ask for ref if not provided
+if [[ -z "$VAULT_VERSION" ]]; then
+    read -rp "Input a git ref (commit hash, branch name, tag name, 'master'): " input
+    VAULT_VERSION="${input}"
+fi
+
+# Check the format of the provided vault version
+# If this is vYYYY.M.B or YYYY.M.B then fix this automatically to prepend web- or web-v
+if [[ "${VAULT_VERSION}" =~ ^20[0-9]{2}\.[0-9]{1,2}.[0-9]{1} ]]; then
+    VAULT_VERSION="web-v${VAULT_VERSION}"
+elif [[ "${VAULT_VERSION}" =~ ^v20[0-9]{2}\.[0-9]{1,2}.[0-9]{1} ]]; then
+    VAULT_VERSION="web-${VAULT_VERSION}"
+fi
+echo "Using: '${VAULT_VERSION}' to checkout bitwarden/client."
+
+if [ ! -d "${VAULT_FOLDER}" ]; then
+    # If this is the first time, clone the project
+    git clone --depth=1 https://github.com/bitwarden/clients.git "${VAULT_FOLDER}"
+fi
+
+# If there already is a checked-out repo, lets clean it up first.
+pushd "${VAULT_FOLDER}"
+    git config remote.origin.tagOpt --no-tags
+    # Stash current changes if there are any, we don't want to loose our work if we had some
+    git stash --include-untracked --quiet &> /dev/null || true
+    # Checkout the master repo first
+
+    # Fetch and Checkout the branch we want
+    git fetch --depth 1 origin "${VAULT_VERSION}"
+    git -c advice.detachedHead=false checkout FETCH_HEAD
+popd
